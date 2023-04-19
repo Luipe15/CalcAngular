eab22efcbe7aa1c53c8441c76046c941d476984c
C1328820 Luis Felipe Pinheiro Santos de Araujo Silva <c1328820@interno.bb.com.br>
2023-04-19 17:55:47 -0300

    update of the codes

---
+++ scripts.js
@@ -0,0 +1,83 @@
+class Calculator {
+  constructor(firstDisplay, secondDisplay) {
+    this.firstDisplay = firstDisplay;
+    this.secondDisplay = secondDisplay;
+    this.clear();
+  }
+
+  clear() {
+    this.firstDisplay = "";
+    this.secondDisplay = "";
+    this.operation = undefined;
+  }
+
+  delete() {
+    this.currentOperand = this.currentOperand.toString().slice(0, -1);
+  }
+
+  appendNumber() {
+    if (number === "." && this.secondDisplay.includes(".")) return;
+    this.secondDisplay = this.secondDisplay.toString() + number.toString();
+  }
+
+  chooseOperation(operation) {
+    if (this.currentOperand === "") return;
+    if (this.previousOperand !== "") {
+      this.compute();
+    }
+    this.operation = operation;
+    this.previousOperand = currentOperand;
+    this.currentOperand = "";
+  }
+
+  getDisplayNumber(number) {
+    const stringNumber = number.toString();
+    const integerDigits = parseFloat(stringNumber.split(".")[0]);
+    const decimalDigits = stringNumber.split(".")[1];
+    let integerDisplay;
+    if (inNaN(integerDigits)) {
+      integerDisplay = "";
+    } else {
+      integerDisplay = integerDigits.toLocaleString("pt", {
+        maximumFractionDigits: 0,
+      });
+    }
+    if (decimalDigits != null) {
+      return `${integerDisplay}.${decimalDigits}`;
+    } else {
+      return integerDisplay;
+    }
+  }
+
+  updateDisplay() {
+    console.log("Passou?");
+
+    this.secondDisplay.innerText = this.getDisplayNumber(this.currentOperand);
+    if (this.operation != null) {
+      this.firstDisplay.innerText = `${this.getDisplayNumber(
+        this.previousOperand
+      )} ${this.operation}`;
+    } else {
+      this.firstDisplay.innerText = "";
+    }
+  }
+}
+
+const buttonNumber = document.querySelectorAll("button#number");
+const buttonEqual = document.getElementById("equals");
+const firstDisplay = document.getElementById("display1");
+const secondDisplay = document.getElementById("display2");
+
+const calculator = new Calculator(firstDisplay, secondDisplay);
+
+buttonNumber.forEach((button) => {
+  button.addEventListener("click", () => {
+    console.log(button.value);
+    calculator.appendNumber(button.innerText);
+    calculator.updateDisplay();
+  });
+});
+
+buttonEqual.addEventListener("click", (button) => {
+  calculator.updateDisplay();
+});
---
+++ scripts.ts
@@ -0,0 +1,5 @@
+var app = angular.module('myCalculator', []);
+
+app.controller('myControll', function($scope) {
+    $scope.firstNumber = 
+})
